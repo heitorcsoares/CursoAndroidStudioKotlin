236fd945eb889001c0e60f6e03a9d564d4807300
Heitor Soares <62906388+heitorcsoares@users.noreply.github.com>
2024-01-30 10:08:42 -0300

    Atualização README.kt

--- Modulo17/Aula_2_3_API_REST/app/src/main/java/com/example/hqawasomeapp/README.kt
+++ Modulo17/Aula_2_3_API_REST/app/src/main/java/com/example/hqawasomeapp/README.kt
@@ -2,9 +2,8 @@ package com.example.hqawasomeapp
 
 class README {
 /**
-• Projeto Modulo 16 Aula 01, 02, 03.
-• Corrigido erros de navegação da (Itens) para (Detalhes).
-• Aplicando State com ENUM e LiveDatas
+• Projeto Modulo 17 Aula 01, 02, 03.
+
 
 
 
