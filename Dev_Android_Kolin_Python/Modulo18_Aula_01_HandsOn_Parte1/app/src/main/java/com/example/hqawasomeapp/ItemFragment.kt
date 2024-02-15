995e8c648eef7bde4c0116e5bc68696638054465
Heitor Soares <62906388+heitorcsoares@users.noreply.github.com>
2024-02-15 15:59:34 -0300

    Modulo 18.1

---
+++ Dev_Android_Kolin_Python/Modulo18_Aula_01_HandsOn_Parte1/app/src/main/java/com/example/hqawasomeapp/ItemFragment.kt
@@ -0,0 +1,4 @@
+package com.example.hqawasomeapp
+
+class ItemFragment {
+}
---
+++ Dev_Android_Kolin_Python/Modulo18_Aula_01_HandsOn_Parte1/app/src/main/java/com/example/hqawasomeapp/README.kt
@@ -0,0 +1,15 @@
+package com.example.hqawasomeapp
+
+class README {
+/**
+•• Projeto Modulo 18.1 Aula 1: Hands On - Parte 1
+ -
+
+
+
+
+
+
+
+*/
+}
