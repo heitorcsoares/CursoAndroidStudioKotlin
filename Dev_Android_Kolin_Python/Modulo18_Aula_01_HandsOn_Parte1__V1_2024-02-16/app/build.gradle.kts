d95e33ff7452b752c381f046f305d3f457b62e0d
Heitor Soares <62906388+heitorcsoares@users.noreply.github.com>
2024-02-16 12:16:28 -0300

    Modulo 18 Aula 1 - 16/02/2024 • Recomeçando o projeto - 16/02/2024 • Verificado erro na linha:   implementation("com.squareup.moshi:moshi-Kotlin:1.13.0")   enviado para tutor. - 16/02/2024 • Aguardando resposta do tutor.   continuando a aplicar aula.

--- Dev_Android_Kolin_Python/Modulo18_Aula_01_HandsOn_Parte1__V1_2024-02-16/app/build.gradle.kts
+++ Dev_Android_Kolin_Python/Modulo18_Aula_01_HandsOn_Parte1__V1_2024-02-16/app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
@@ -56,21 +56,21 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Lifecycle components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.core:core-ktx:1.12.0")
 
     // navigation components
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     // UI
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("me.relex:circleindicator:2.1.6")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("org.imaginativeworld.whynotimagecarousel:whynotimagecarousel:2.1.0")        //Carousel de imagens
 
