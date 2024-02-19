2116155c7b2e3d5be17a4b3773660a79d2b9a63c
Heitor Soares <62906388+heitorcsoares@users.noreply.github.com>
2024-02-19 11:17:15 -0300

    Exercicio Modulo 17 em desenvolvimento. - 15/02/2024: enviado para verificar solução. - 19/02/2024: Efetuamos as correções do Tutor, porem apresentou "Failed to resolver", Falha ao resolver. - 19/02/2024: Abrindo nova solicitação ao Tutor.

--- Dev_Android_Kolin_Python/Modulo17_Exercicio/app/build.gradle.kts
+++ Dev_Android_Kolin_Python/Modulo17_Exercicio/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -45,9 +45,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
@@ -55,27 +55,27 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Lifecycle components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
 
     // navigation components
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     // UI
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("org.imaginativeworld.whynotimagecarousel:whynotimagecarousel:2.1.0")        //Carousel de imagens
 
-    // RETROFIT
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.moshi:moshi-Kotlin:1.13.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
-    implementation("com.github.bumptech.glide:glide:4.13.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")
+    // RETROFIT e MOSHI
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")                 // RETROFIT
+    implementation("com.squareup.moshi:moshi-Kotlin:1.12.0-rc2")                // MOSHI
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")          // RETORFIT com MOSHI
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.12.0-rc2")                  //KAP
+    implementation("com.github.bumptech.glide:glide:4.13.1")                //GLIDE
+    annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")        //GLIDE
 }
--- Dev_Android_Kolin_Python/Modulo17_Exercicio/app/src/main/java/com/example/filmes/ApiCredentials.kt
+++ Dev_Android_Kolin_Python/Modulo17_Exercicio/app/src/main/java/com/example/filmes/ApiCredentials.kt
@@ -2,16 +2,4 @@ package com.example.filmes
 
 class ApiCredentials {
 
-    val api_key = ecdd57b00e263182164add9d75414318
-
-    val client = OkHttpClient()
-
-    val request = Request.Builder()
-        .url("https://api.themoviedb.org/3/movie/latest")
-        .get()
-        .addHeader("accept", "application/json")
-        .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlY2RkNTdiMDBlMjYzMTgyMTY0YWRkOWQ3NTQxNDMxOCIsInN1YiI6IjY1YjdlZDM0MGZiMTdmMDE2MjM0MWQyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JLwWPS-2WH22bQzDa8mN83wXt_IIdSC_4VCp38bnj9M")
-        .build()
-
-    val response = client.newCall(request).execute()
 }
