4491beb9ea0a1a78993e762adcd1d57d9d6705ff
Heitor Soares <62906388+heitorcsoares@users.noreply.github.com>
2024-02-28 16:56:31 -0300

    ____________________________________ Modulo 18 Aula 2 ******************* - 19/02/2024 • Inicio aula 2 - 20/02/2024 • Rsolvendo importação ApiCredentials.kt em HQViewModel.kt - 20/02/2024 • Abrindo pergunta ao Tutor - 22/02/2024 • Terminai as 2 aulas, porem tem erro: HQFragment.kt HQFragmentDirections está em vermelho. Abrindo consulta com Tutor. -27/02/2024 • Corrigimos todos os erros, pore´m o App abre e fecha sozinho.
    ____________________________________
    Modulo 18 Aula 1 *******************
    - 16/02/2024 • Recomeçando o projeto
    - 16/02/2024 • Verificado erro na linha:
      implementation("com.squareup.moshi:moshi-Kotlin:1.13.0")
      enviado para tutor.
    - 19/02/2024 • Todo conteudo da M18Aula 1 Funcionou

--- Dev_Android_Kolin_Python/Modulo18_Aula_02_HandsOn_Parte2/app/build.gradle.kts
+++ Dev_Android_Kolin_Python/Modulo18_Aula_02_HandsOn_Parte2/app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")        //GLIDE
 
 
-    /** IMPLEMENTAÇÃO NAVEGAÇÃO
+    /** IMPLEMENTAÇÃO NAVEGAÇÃO */
     // Kotlin
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
@@ -97,5 +97,5 @@ dependencies {
 
     // Jetpack Compose Integration
     implementation("androidx.navigation:navigation-compose:2.7.7")
-     */
+
 }
