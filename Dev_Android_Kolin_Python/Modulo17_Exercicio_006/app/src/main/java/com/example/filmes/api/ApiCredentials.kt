29035842c79663c3e588f357f2f698898b3965fe
Heitor Soares <heitor.c.soares@gmail.com>
2024-07-24 16:16:56 -0300

    Manutenção: Apagamos pastas de Backup Modu17 Exercicio 001 - 005 - 006

---
+++ Dev_Android_Kolin_Python/Modulo17_Exercicio_006/app/src/main/java/com/example/filmes/api/ApiCredentials.kt
@@ -0,0 +1,12 @@
+package com.example.filmes.api
+
+class ApiCredentials {
+
+    companion object {
+        const val API_KEY = "b2b5010b0352ebd874753d40f095a118"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val IMG_POSTER = "https://image.tmdb.org/t/p/original"
+        const val AUTHENTICATION = "https://api.themoviedb.org/3/authentication"
+    }
+
+}
---
+++ Dev_Android_Kolin_Python/Modulo17_Exercicio_006/app/src/main/java/com/example/filmes/viewBinder/viewBinder.kt
@@ -0,0 +1,20 @@
+package com.example.filmes.viewBinder
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+/** Adaptador para visualização da imagem. (Configurações da Imagem) */
+@BindingAdapter("srcUrl")
+fun ImageView.bindScrUrl(
+    url: String
+){
+
+    Glide                                 //Carregamento de Imagem
+        .with(this)                 //Local aonde está carregando a imagem (THIS) fragemento que está sendo executada
+        .load(url)                       //Variavel url informa o endereço da imagem
+        .centerInside()                  //imagem centralizada mantendo sua proporção
+        .into(this)                 //local aonde a imagem será exibida (this) no local que executou o Adaptador
+
+
+}
