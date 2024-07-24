b3e960788f9b006e050da44cb3117ffc4dc1d951
Heitor Soares <heitor.c.soares@gmail.com>
2024-07-24 16:04:01 -0300

    Exercicio mod-19: - Corrigido endereço da imagem - Lista concluida com: Titlo/conteudo/imagem. Proximo passo: - Tela Detalhes - Botão coltar

--- Dev_Android_Kolin_Python/Modulo17_Exercicio_005/app/src/main/java/com/example/filmes/api/ApiCredentials.kt
+++ Dev_Android_Kolin_Python/Modulo17_Exercicio_005/app/src/main/java/com/example/filmes/api/ApiCredentials.kt
@@ -5,8 +5,7 @@ class ApiCredentials {
     companion object {
         const val API_KEY = "b2b5010b0352ebd874753d40f095a118"
         const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val LISTA_URL = "https://api.themoviedb.org/3/movie/latest"
-        const val IMG_POSTER = "https://api.themoviedb.org/3/movie/now_playing"
+        const val IMG_POSTER = "https://image.tmdb.org/t/p/original"
         const val AUTHENTICATION = "https://api.themoviedb.org/3/authentication"
     }
 
